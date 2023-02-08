4b3216daab5670bf3364b9541f5fdd2e2bf5fb27
cwanserGT <110735970+christianwanser@users.noreply.github.com>
2023-02-07 16:09:34 -0800

    Update li_content_creators.py

--- li_content_creators.py
+++ li_content_creators.py
@@ -94,6 +94,8 @@ def main_page():
     
     survey_qs
     
+    survey_qs["New Q"][0]
+    
     st.write("How long have you been creating data content on LinkedIn?")
 
     col1,col2,col3,col4,col5,col6,col7,col8 = st.columns([1,1,1,1,1,1,1,1])
