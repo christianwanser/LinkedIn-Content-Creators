2900e4434374fdfd501f65e8eb7996d3c4536bd8
cwanserGT <110735970+christianwanser@users.noreply.github.com>
2023-02-07 16:47:35 -0800

    Update li_content_creators.py

--- li_content_creators.py
+++ li_content_creators.py
@@ -70,7 +70,8 @@ survey_qs["New Q"] = [re.findall('\[(.*?)\]', x)[0] if re.findall('\[(.*?)\]', x
 # Define Answer Options
 
 survey_qs["Options"] = [["0%","1-25%","26-50%","51-75%","76-100%"] if "percent" in x else "" for x in survey_qs["Original Q"]]
-followers = survey_qs.loc[survey_qs["Original Q"].str.contains("followers", case=False)]
+#followers = survey_qs.loc[survey_qs["Original Q"].str.contains("followers", case=False)]
+followers = survey_qs.index[survey_qs["Original Q"].str.contains("followers", case=False)].tolist()
 st.write(followers.index())
 
 
